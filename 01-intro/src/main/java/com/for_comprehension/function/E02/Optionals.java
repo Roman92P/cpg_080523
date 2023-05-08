5a077079c719f89c1ca58885b446124254078ed8
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-05-08 14:43:45 +0200

    4. Optional - solutions (1/2)

--- 01-intro/src/main/java/com/for_comprehension/function/E02/Optionals.java
+++ 01-intro/src/main/java/com/for_comprehension/function/E02/Optionals.java
@@ -9,6 +9,8 @@ import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static java.util.function.Predicate.not;
+
 class Optionals {
 
     private static final Logger log = LoggerFactory.getLogger(Optionals.class);
@@ -16,69 +18,66 @@ class Optionals {
     private static final String DEFAULT = "DEFAULT";
 
     /**
-     * Call {@link this#findOneById } with a provided id and handle absence by throwing {@link NoSuchElementException}
-     * Hint: {@link Optional#get()}
+     * Call {@link this#findOneById } with a provided id and handle absence by throwing
+     * {@link NoSuchElementException} Hint: {@link Optional#get()}
      *
      * WARNING: this is an anti-pattern (unless you know what you're doing)
      */
     static Function<Integer, Person> L1_bruteForceGet() {
-        return id -> {
-            throw new RuntimeException("TODO");
-        };
+        return id -> findOneById(id).get();
     }
 
     /**
-     * Call {@link this#findOneById } with a provided id and handle absence by throwing {@link IllegalStateException}
-     * Hint: check {@link Optional}'s API
-     *
+     * Call {@link this#findOneById } with a provided id and handle absence by throwing
+     * {@link IllegalStateException} Hint: check {@link Optional}'s API
      */
     static Function<Integer, Person> L2_customException() {
-        return id -> {
-            throw new RuntimeException("TODO");
-        };
+        return id -> findOneById(id).orElseThrow(IllegalStateException::new);
     }
 
     /**
      * Get {@link Person#name} if found or else return {@link this#DEFAULT}
      */
     static Function<Integer, String> L3_defaultValue() {
-        return id -> {
-            throw new RuntimeException("TODO");
-        };
+        return id -> findOneById(id)
+            .map(Person::getName)
+            .filter(str -> !str.isBlank())
+            .orElse(DEFAULT);
     }
 
     /**
-     * Get {@link Person#name} if found or else return the value returned by provided method (represented by Supplier<String)
+     * Get {@link Person#name} if found or else return the value returned by provided method (represented by
+     * Supplier<String)
      */
     static BiFunction<Integer, Supplier<String>, String> L4_defaultValueMethodResult() {
-        return (id, function) -> {
-            throw new RuntimeException("TODO");
-        };
+        return (id, supplier) -> findOneById(id)
+            .map(Person::getName)
+            .filter(str -> !str.isBlank())
+            .orElseGet(supplier);
     }
 
     /**
-     * Get {@link Person#name}, uppercase it,
-     * if empty string, treat as absent
-     * or else return a default value
-     * Hint: {@link Optional#filter}
+     * Get {@link Person#name}, uppercase it, if empty string, treat as absent or else return a default value Hint:
+     * {@link Optional#filter}
      */
     static Function<Integer, String> L5_processValue() {
-        return id -> {
-            throw new RuntimeException("TODO");
-        };
+        return id -> findOneById(id)
+            .map(Person::getName)
+            .filter(not(String::isBlank))
+            .map(String::toUpperCase)
+            .orElse(DEFAULT);
     }
 
     /**
-     * Get {@link Person#name}
-     * if empty string, treat as absent
-     * or else return a default value (42)
-     * and then find age by calling {@link this#findAgeByName(String)}
-     * Hint: {@link Optional#flatMap(Function)}
+     * Get {@link Person#name} if empty string, treat as absent or else return a default value (42) and then find
+     * age by calling {@link this#findAgeByName(String)} Hint: {@link Optional#flatMap(Function)}
      */
     static Function<Integer, Integer> L6_nestedOptionals() {
-        return id -> {
-            throw new RuntimeException("TODO");
-        };
+        return id -> findOneById(id)
+            .map(Person::getName)
+            .filter(not(String::isBlank))
+            .flatMap(Optionals::findAgeByName)
+            .orElse(42);
     }
 
     static class Person {
@@ -94,6 +93,7 @@ class Optionals {
         String getName() {
             return name;
         }
+
     }
 
     private static Optional<Integer> findAgeByName(String name) {
@@ -104,4 +104,5 @@ class Optionals {
         log.info("Fetching person for id [{}]", id);
         return id == 42 ? Optional.of(new Person("Andre")) : Optional.empty();
     }
+
 }
