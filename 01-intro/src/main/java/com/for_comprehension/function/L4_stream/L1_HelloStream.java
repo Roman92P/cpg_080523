6ed504e11f023c31d2d9ed11fbe2e328daf06d44
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-05-08 16:15:24 +0200

    6. Stream Basics - create

--- 01-intro/src/main/java/com/for_comprehension/function/L4_stream/L1_HelloStream.java
+++ 01-intro/src/main/java/com/for_comprehension/function/L4_stream/L1_HelloStream.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class HelloStream {
+public class L1_HelloStream {
 
     public static void main(String[] args) {
     }
