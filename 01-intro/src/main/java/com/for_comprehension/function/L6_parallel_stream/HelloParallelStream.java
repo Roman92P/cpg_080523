0218c6358a68996e5ace397249a7a65d39e38bea
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-05-09 16:06:33 +0200

    10. Thread - basics

---
+++ 01-intro/src/main/java/com/for_comprehension/function/L6_parallel_stream/HelloParallelStream.java
@@ -0,0 +1,28 @@
+package com.for_comprehension.function.L6_parallel_stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class HelloParallelStream {
+
+    public static void main(String[] args) {
+        List<Integer> ints = IntStream.range(0, 20).boxed().collect(Collectors.toList());
+
+        List<Integer> collect = ints.parallelStream()
+            .map(HelloParallelStream::process)
+            .collect(Collectors.toList());
+    }
+
+    public static <T> T process(T input) {
+        System.out.println("processing... " + input);
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e) {
+        }
+        return input;
+    }
+
+}
